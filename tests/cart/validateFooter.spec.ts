eff84ef267e6ee063fe29db0a13ca3e2aa229a3d
Chong S. Lee <chong@Chongs-MacBook-Air.local>
2026-06-29 15:37:15 -0700

    feat: test to check twitter link

--- tests/cart/validateFooter.spec.ts
+++ tests/cart/validateFooter.spec.ts
@@ -1,6 +1,7 @@
 import { expect } from '@playwright/test';
 import { test } from '../../fixtures/fixtures';
 import { SOCIAL_LINKS } from '../../constants/socialLinks';
+import { ManagerPage } from '../../pages/manager/ManagerPage';
 
 test.describe('Footer Link Year Validation', () => {
   test.beforeEach(async ({ config, managerPage }) => {
@@ -9,9 +10,6 @@ test.describe('Footer Link Year Validation', () => {
 
   test('Validate all social media links and the current year displayed on the Inventory page.', async ({ managerPage }) => {
     const inventoryPage = managerPage.onInventoryPage();
-    const cartPage = managerPage.onCartPage();
-    const checkout = managerPage.onCheckout();
-
     await inventoryPage.footer.expectCurrentYear();
     for (const link of SOCIAL_LINKS) {
       const currentPage = await inventoryPage.footer.openSocialLink(link.name)
@@ -19,6 +17,17 @@ test.describe('Footer Link Year Validation', () => {
     }
   });
 
+  test('Click the link inside twitter page', async ({ managerPage }) => {
+    const inventoryPage = managerPage.onInventoryPage();
+    const twitterPage = await inventoryPage.footer.openSocialLink('Twitter')
+    await twitterPage.getByRole('link', { name: 'saucelabs.com' }).click();
+    await expect(twitterPage).toHaveURL('https://saucelabs.com/');
+    await twitterPage.close();
+  })
+
+});
+
+test.describe('Header Link Validation', () => {
   test('Validate Header Layout displayed on the Inventory page.', async ({ managerPage }) => {
     const inventoryPage = managerPage.onInventoryPage();
     await expect(inventoryPage.header.getBurgerMenu().reactBtnMenu).toMatchAriaSnapshot(`- button "Open Menu"`);
